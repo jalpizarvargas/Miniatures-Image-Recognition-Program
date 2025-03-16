a393d771284bb3df461b7d44b19508b1c51019fc
jalpizarvargas <jpalpizarcr@gmail.com>
2025-03-16 12:45:57 -0400

    first commit

---
+++ mini_image_recognition.py
@@ -0,0 +1,9 @@
+#Read Images and Tags, using supervised learning
+
+#Ensure all images are at the same resolution
+
+#Run the images through wavelet transform to disect the image for specific characteristics
+
+#Run this images through a classification algorithm 
+
+#Determine performance metrics and analyze results
